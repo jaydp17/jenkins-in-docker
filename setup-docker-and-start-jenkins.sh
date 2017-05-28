e718418f5e2f7d34b1da4d33ad92237299217146
Jaydeep Solanki <jaydp17@gmail.com>
2017-05-28 18:41:05 +0530

    convert tabs to spaces in setup-docker-and-start-jenkins.sh

--- setup-docker-and-start-jenkins.sh
+++ setup-docker-and-start-jenkins.sh
@@ -9,23 +9,23 @@ JUSER="jenkins"
 DOCKER_GID=$(ls -aln /var/run/docker.sock  | awk '{print $4}')
 
 if ! getent group $DOCKER_GID; then
-	echo creating docker group $DOCKER_GID
-	addgroup --gid $DOCKER_GID docker
+  echo creating docker group $DOCKER_GID
+  addgroup --gid $DOCKER_GID docker
 fi
 
 if ! getent group $GID; then
-	echo creating $JUSER group $GID
-	addgroup --gid $GID $JUSER
+  echo creating $JUSER group $GID
+  addgroup --gid $GID $JUSER
 fi
 
 if ! getent passwd $JUSER; then
-	echo useradd -N --gid $GID -u $UID $JUSER
-	useradd -N --gid $GID -u $UID $JUSER
+  echo useradd -N --gid $GID -u $UID $JUSER
+  useradd -N --gid $GID -u $UID $JUSER
 fi
 
 DOCKER_GROUP=$(ls -al /var/run/docker.sock  | awk '{print $4}')
 if ! id -nG "$JUSER" | grep -qw "$DOCKER_GROUP"; then
-	adduser $JUSER $DOCKER_GROUP
+  adduser $JUSER $DOCKER_GROUP
 fi
 
 chown -R $JUSER:$JUSER /var/jenkins_home/
